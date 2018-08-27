8f73e57a67c77c8035f25ba74327acfe11584115
Vivian To <toxxx045@umn.edu>
2018-08-27 15:13:36 -0500

    edit java script

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
+$(document).ready(function(){
+    $('.parallax-window:nth-child(2)').parallax({
+        imageSrc: "img/auto-dose-render.png"
+    });
 
+    $('.parallax-window:nth-child(2)').parallax({
+        imageSrc: "img/sled.jpg"
+    });
+}
 
-$('.parallax-window:nth-child(2)').parallax({
-    imageSrc: "img/auto-dose-render.png"
-});
-
-$('.parallax-window:nth-child(2)').parallax({
-    imageSrc: "img/sled.jpg"
-});
+// $('.parallax-window:nth-child(2)').parallax({
+//     imageSrc: "img/auto-dose-render.png"
+// });
+//
+// $('.parallax-window:nth-child(2)').parallax({
+//     imageSrc: "img/sled.jpg"
+// });
